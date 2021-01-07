d3685b94cea3ab04429a5a7d04a92c865cd9a8d8
Mukesh Suthar <mukeshsuthar336@gmail.com>
2021-01-07 18:32:22 +0000

    feat: subcollections are created if any nested folder found

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 /* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable no-param-reassign */
 /* eslint-disable import/no-unresolved */
@@ -6,7 +7,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable global-require */
-import { Request, NextFunction, Application } from 'express';
+import express, { Request, NextFunction, Application } from 'express';
 
 import * as path from 'path';
 import * as fs from 'fs';
@@ -24,6 +25,7 @@ import { schemaValidator, getStatusCode } from './validator';
  */
 const createRoute = (params: { route: string; operation: Function; schema: { [key: string]: any }; app: Application }): Application => {
     const { route, operation, schema, app } = params;
+    // console.log(route);
     return app.post(route, async (req: Request, res: any, next: NextFunction) => {
         try {
             // Validate body data with its schema
@@ -58,6 +60,29 @@ const createRoute = (params: { route: string; operation: Function; schema: { [ke
 };
 
 /**
+ * This function will return all collection
+ * as well as sub collections directories path
+ *
+ * @param {String} collectionPath  Collection directory path
+ * @returns {Array} List of directories path.
+ */
+const getListOfAllCollections = () => {
+    const collectionDir: Array<string> = [];
+    return function recurse(collectionPath: string) {
+        fs.readdirSync(collectionPath).map((file) => {
+            const filePath = path.join(collectionPath, file);
+            const stat = fs.statSync(filePath);
+            if (stat.isDirectory()) {
+                recurse(filePath);
+            } else if (file === 'index.js') {
+                collectionDir.push(filePath);
+            }
+        });
+        return collectionDir;
+    };
+};
+
+/**
  * Controller which goes through all collections
  * and create routes based on operations
  * and validate body data based on schemas
@@ -69,22 +94,28 @@ const createRoute = (params: { route: string; operation: Function; schema: { [ke
  */
 const apicontroller = (params: { collectionPath: string; baseUrl: string; app: Application }): null => {
     const { collectionPath, baseUrl, app } = params;
-    const collectionsDir: Array<string> = fs.readdirSync(collectionPath);
+    const collectionsDir: Array<string> = getListOfAllCollections()(collectionPath);
 
     // Check if collection directory is not empty.
     if (collectionsDir.length === 0) throw new Error(`No collections defined under ${collectionPath}`);
 
     collectionsDir.map((collection: string) => {
         // Get Operation and schema from a collection
-        const { operations, schema } = require(path.join(collectionPath, collection))(app);
+        const collectionModule = require(path.join(collection));
+
+        if (typeof collectionModule !== 'function') {
+            throw new Error(`File: "${collection}" does not match the collection file signature, Please refer the document!`);
+        }
+        const { operations, schema } = collectionModule(app);
 
         // Check operations object should not be empty
-        if (Object.keys(operations).length === 0) throw new Error(`No operations defined for ${collection}`);
+        if (!operations || Object.keys(operations).length === 0) throw new Error(`No operations defined for ${collection}`);
 
+        collection = path.dirname(collection.replace(collectionPath, ''));
         // Create route for each operation.
         Object.keys(operations).map((operation: string) => {
             return createRoute({
-                route: `${baseUrl}/${collection}/${operation}/:scope?`,
+                route: `${baseUrl}${collection}/${operation}/:scope?`,
                 operation: operations[operation],
                 schema,
                 app,
@@ -94,4 +125,6 @@ const apicontroller = (params: { collectionPath: string; baseUrl: string; app: A
     });
     return null;
 };
+
+// apicontroller({ collectionPath: path.join(__dirname, '../example/collections'), baseUrl: 'example', app: express() });
 export = apicontroller;
